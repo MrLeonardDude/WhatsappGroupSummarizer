4e88a950a2ce8b55589d49b9c4a379b564b33bcc
MrLeonardDude <leopagnez@gmail.com>
2024-12-08 12:41:49 -0300

    feat: create file group message repo

---
+++ group_summarizer/infrastructure/group_message_repository/interface.py
@@ -0,0 +1,7 @@
+from typing import List
+from group_summarizer.domain.group_message import GroupMessage
+
+class GroupMessageRepository:
+
+    def get_messages(self) -> List[GroupMessage]:
+        return []
